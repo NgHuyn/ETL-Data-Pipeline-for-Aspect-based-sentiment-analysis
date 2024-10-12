8821acc068304b072153052300240ef4818b9690
Tạ Hoàng Kim Thy <21280083@student.hcmus.edu.vn>
2024-10-12 16:16:51 +0700

    update

--- run_crawl.py
+++ run_crawl.py
@@ -3,9 +3,9 @@ from src.crawl_reviews import MovieReviewScraper
 
 if __name__ == "__main__":
     # get the movie_id first
-    scraper = MoviesScraper(clicks=2, release_date_from='2024-01-01', release_date_to='2024-10-07')
+    scraper = MoviesScraper(clicks=1, release_date_from='2024-01-01', release_date_to='2024-10-07')
     movie_data = scraper.fetch_movies()
-    print(movie_data[:5])
+    print(movie_data[:1])
 
     # get the reviews from each movie
     scraper = MovieReviewScraper(movie_data=movie_data[:1])
